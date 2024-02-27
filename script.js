b18572a092350e7e8500448f0d643cf5a7f43ec3
DalleDondale <140009474+DalleDondale@users.noreply.github.com>
2024-02-27 18:47:15 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Generate random RGB color
+// Display color in square
+
+// Generate color options 
+// Add click handler to options
+
+// Check for correct pick
+// Update message
+
+// Reset game on button click
